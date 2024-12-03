5c6e800ca4b5ea724376fdfb8ccdb5be5d8bb6a0
dariantonova <daricio1256@gmail.com>
2024-12-03 17:18:53 +0300

    Add db

---
+++ src/db/db.ts
@@ -0,0 +1,31 @@
+export enum Resolutions {
+    P144 = 'P144',
+    P240 = 'P240',
+    P360 = 'P360',
+    P480 = 'P480',
+    P720 = 'P720',
+    P1080 = 'P1080',
+    P1440 = 'P1440',
+    P2160 = 'P2160',
+}
+
+export type VideoType = {
+    id: number,
+    title: string,
+    author: string,
+    canBeDownloaded: boolean,
+    minAgeRestriction: number | null,
+    createdAt: string,
+    publicationDate: string,
+    availableResolutions: Resolutions[] | null,
+};
+
+export type DBType = { videos: VideoType[] };
+
+export const db: DBType = {
+    videos: [],
+};
+
+export const setDb = (videos: VideoType[] | undefined) => {
+    db.videos = videos ? videos : [];
+};
