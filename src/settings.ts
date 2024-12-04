f96e38a4059d118e93d05770664c3e50b91df73c
dariantonova <daricio1256@gmail.com>
2024-12-04 09:20:57 +0300

    Change path for testing

--- src/settings.ts
+++ src/settings.ts
@@ -6,6 +6,6 @@ export const SETTINGS = {
     PORT: process.env.PORT || 3003,
     PATH: {
         VIDEOS: '/hometask_01/api/videos',
-        TESTING: '/hometask_01/api/testing/all-data',
+        TESTING: '/hometask_01/api/testing',
     }
 }
