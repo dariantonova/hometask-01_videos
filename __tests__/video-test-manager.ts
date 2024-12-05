a55217475e95a7636159dbdd53751b0a36d25f35
dariantonova <daricio1256@gmail.com>
2024-12-05 12:41:12 +0300

    Add tests for update video endpoint

--- __tests__/video-test-manager.ts
+++ __tests__/video-test-manager.ts
@@ -27,4 +27,36 @@ export const videoTestManager = {
             expect(isNaN(new Date(createdVideo.publicationDate).getTime())).toBe(false);
         }
     },
+    async updateVideo(id: number, data: any, expectedStatusCode: number) {
+        const getVideoResponse = await req
+            .get(SETTINGS.PATH.VIDEOS + '/' + id);
+        const video: VideoViewModel = getVideoResponse.body;
+
+        await req
+            .put(SETTINGS.PATH.VIDEOS + '/' + id)
+            .send(data)
+            .expect(expectedStatusCode);
+
+        if (expectedStatusCode === 204) {
+            const getUpdatedVideoResponse = await req
+                .get(SETTINGS.PATH.VIDEOS + '/' + id)
+                .expect(HTTP_STATUSES.OK_200);
+            const updatedVideo: VideoViewModel = getUpdatedVideoResponse.body;
+
+            expect(updatedVideo).toEqual({
+                id: video.id,
+                title: data.title,
+                author: data.author,
+                canBeDownloaded: typeof data.canBeDownloaded !== 'undefined' ? data.canBeDownloaded
+                    : video.canBeDownloaded,
+                minAgeRestriction: typeof data.minAgeRestriction !== 'undefined' ? data.minAgeRestriction
+                    : video.minAgeRestriction,
+                createdAt: video.createdAt,
+                publicationDate: data.publicationDate ? new Date(data.publicationDate).toISOString()
+                    : video.publicationDate,
+                availableResolutions: typeof data.availableResolutions !== 'undefined' ? data.availableResolutions
+                    : video.availableResolutions,
+            });
+        }
+    },
 };
--- __tests__/videos.e2e.test.ts
+++ __tests__/videos.e2e.test.ts
@@ -4,6 +4,7 @@ import {HTTP_STATUSES} from "../src/utils";
 import {setDb, VideoType} from "../src/db/db";
 import {mapVideoToViewModel} from "../src/routes/videos.router";
 import {videoTestManager} from "./video-test-manager";
+import {UpdateVideoInputModel} from "../src/models/UpdateVideoInputModel";
 
 describe('tests for /videos', () => {
     beforeAll(async () => {
@@ -141,6 +142,86 @@ describe('tests for /videos', () => {
 
         for (const dataItem of data) {
             await videoTestManager.createVideo(dataItem, HTTP_STATUSES.CREATED_201);
+        }
+    });
+
+    it('should return 404 when updating non-existing video', async () => {
+        const data: UpdateVideoInputModel = {
+            title: 'video',
+            author: 'author',
+        };
+        await videoTestManager.updateVideo(-100, data, HTTP_STATUSES.NOT_FOUND_404);
+    });
+
+    it(`shouldn't update video with incorrect input data`, async () => {
+        const data = [];
+
+        const data1 = {};
+        data.push(data1);
+
+        const data2 = {
+            title: '  ',
+            author: '',
+        };
+        data.push(data2);
+
+        const data3 = {
+            title: 'title',
+            author: 'author',
+            availableResolutions: [],
+        };
+        data.push(data3);
+
+        const data4 = {
+            title: 'title',
+            author: 'author',
+            canBeDownloaded: 4,
+        };
+        data.push(data4);
+
+        const data5 = {
+            title: 'title',
+            author: 'author',
+            minAgeRestriction: -1,
+        };
+        data.push(data5);
+
+        const data6 = {
+            title: 'title',
+            author: 'author',
+            publicationDate: 'not date',
+        };
+        data.push(data6);
+
+        for (const dataItem of data) {
+            await videoTestManager.updateVideo(1, dataItem, HTTP_STATUSES.BAD_REQUEST_400);
+        }
+    });
+
+    it('should update video with correct input data', async () => {
+        const data: UpdateVideoInputModel[] = [];
+
+        const data1: UpdateVideoInputModel = {
+            title: 'title',
+            author: 'author',
+            availableResolutions: ['P1080'],
+            canBeDownloaded: true,
+            minAgeRestriction: 16,
+            publicationDate: '2024',
+        };
+        data.push(data1);
+
+        const data2: UpdateVideoInputModel = {
+            title: 'new title',
+            author: 'author',
+            availableResolutions: null,
+            canBeDownloaded: false,
+            minAgeRestriction: null,
+        };
+        data.push(data2);
+
+        for (const dataItem of data) {
+            await videoTestManager.updateVideo(1, dataItem, HTTP_STATUSES.NO_CONTENT_204);
         }
     });
 });
